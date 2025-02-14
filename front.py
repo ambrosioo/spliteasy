ef4b50594857eef2e20efe76e5c32944f1c8e421
Matheus Ambrósio <105406131+ambrosioo@users.noreply.github.com>
2025-02-14 16:44:55 -0300

    front.py

--- front.py
+++ front.py
@@ -5,13 +5,15 @@ import os
 # Título da aplicação
 st.title("SplitEasy - Divisor de Arquivos CSV")
 
+# Caminho da pasta Downloads
+downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
+
 # Upload do arquivo CSV
 uploaded_file = st.file_uploader("Selecione um arquivo CSV", type=["csv"])
 
 if uploaded_file is not None:
     # Solicitar o número de linhas por arquivo
-    chunk_size = st.number_input(
-        "Quantas linhas por arquivo?", min_value=1, value=100)
+    chunk_size = st.number_input("Quantas linhas por arquivo?", min_value=1, value=100)
 
     # Botão para dividir o arquivo
     if st.button("Dividir Arquivo"):
@@ -19,13 +21,12 @@ if uploaded_file is not None:
         df = pd.read_csv(uploaded_file, dtype=str)
 
         # Calcular o número de partes
-        num_chunks = (len(df) // chunk_size) + \
-            (1 if len(df) % chunk_size != 0 else 0)
+        num_chunks = (len(df) // chunk_size) + (1 if len(df) % chunk_size != 0 else 0)
 
         # Dividir o arquivo e salvar as partes
-        for i, chunk in enumerate(range(0, len(df), chunk_size)):  # Corrigido aqui
+        for i, chunk in enumerate(range(0, len(df), chunk_size)):
             chunk_df = df.iloc[chunk:chunk + chunk_size]
-            output_file = f"parte_{i + 1}.csv"
+            output_file = os.path.join(downloads_path, f"parte_{i + 1}.csv")  # Salvar na pasta Downloads
             chunk_df.to_csv(output_file, index=False)
             st.write(f"Arquivo salvo: {output_file}")
 
